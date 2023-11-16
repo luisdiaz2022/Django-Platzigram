8fd3362bf924643305138db04beb6b23d67b2908
Luis <luisedgucv@gmail.com>
2023-11-15 21:31:16 -0500

    removed html file, and fixed photos size

--- posts/views.py
+++ posts/views.py
@@ -11,28 +11,28 @@ posts = [
         'title': 'Mont Blanc',
         'user': {
             'name': 'Yesica Cortes',
-            'picture': 'https://picsum.photos/200/200/?image=1027'
+            'picture': 'https://picsum.photos/60/60/?image=1027'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=1036',
+        'photo': 'https://picsum.photos/800/600/?image=1036',
     },
     {
         'title': 'Via Lactea',
         'user': {
             'name': 'Christian van der Henst',
-            'picture': 'https://picsum.photos/200/200/?image=1005'
+            'picture': 'https://picsum.photos/60/60/?image=1005'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=903',
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
         'title': 'Nuevo Auditorio',
         'user': {
             'name': 'Uriel (thespianartist)',
-            'picture': 'https://picsum.photos/200/200/?image=883'
+            'picture': 'https://picsum.photos/60/60/?image=883'
         },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/200/200/?image=1076',
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
