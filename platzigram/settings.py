5bb3b69af51e97fac16070325c04038c23390809
Luis <luisedgucv@gmail.com>
2023-11-28 09:19:48 -0500

    fixed issue with the login_required decorator, defined correct url to redirect

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
-LOGIN_URL = '/users/login/'
+LOGIN_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
