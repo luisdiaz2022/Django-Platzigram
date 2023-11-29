6c1874dbef1a644f9a13462f5f1b7465984c4900
Luis <luisedgucv@gmail.com>
2023-11-28 21:31:39 -0500

    Added signup view, added template and added url for it

--- users/views.py
+++ users/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Exception
+from django.db.utils import IntegrityError
+
 # Create your views here.
 
 def login_view(request):
@@ -26,3 +33,30 @@ def logout_view(request):
     """Logout a user."""
     logout(request)
     return redirect('login')
+
+def signup(request):
+    """Sign up view."""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
