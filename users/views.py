10b8f5fe78f540ca2779e1f72bb1625b846bf73f
Luis <luisedgucv@gmail.com>
2023-11-28 22:15:17 -0500

    added profile view and added middleware to handle user without all the info required

--- users/views.py
+++ users/views.py
@@ -14,6 +14,11 @@ from django.db.utils import IntegrityError
 
 # Create your views here.
 
+@login_required
+def update_profile(request):
+    """Update an user's profile view."""
+    return render(request, 'users/update_profile.html')
+
 def login_view(request):
     """Login view"""
     if request.method == 'POST':
