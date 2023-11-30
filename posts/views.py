99f40a15827472da9f8fe14b646df26ab3bdd531
Luis <luisedgucv@gmail.com>
2023-11-30 15:58:06 -0500

    added template and modified the view to render the post in sqlite

--- posts/views.py
+++ posts/views.py
@@ -8,42 +8,15 @@ from django.contrib.auth.decorators import login_required
 
 from posts.forms import PostForm
 
-# Utilities
-from datetime import datetime
+# Models
 
-posts = [
-    {
-        'title': 'Mont Blanc',
-        'user': {
-            'name': 'Yesica Cortes',
-            'picture': 'https://picsum.photos/60/60/?image=1027'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/600/?image=1036',
-    },
-    {
-        'title': 'Via Lactea',
-        'user': {
-            'name': 'Christian van der Henst',
-            'picture': 'https://picsum.photos/60/60/?image=1005'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/800/800/?image=903',
-    },
-    {
-        'title': 'Nuevo Auditorio',
-        'user': {
-            'name': 'Uriel (thespianartist)',
-            'picture': 'https://picsum.photos/60/60/?image=883'
-        },
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'photo': 'https://picsum.photos/500/700/?image=1076',
-    }
-]
+from posts.models import Post
+
 
 @login_required
 def list_posts(request):
     """List existing posts."""
+    posts = Post.objects.all().order_by('-created')
     return render(request, 'posts/feed.html', {'posts': posts})
 
 @login_required
