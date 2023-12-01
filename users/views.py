8a142e55484dc94ac5b12567128a14e71bba1931
Luis <luisedgucv@gmail.com>
2023-12-01 16:25:53 -0500

    added all the urls into each app and created ckass-based views for each path

--- users/views.py
+++ users/views.py
@@ -26,7 +26,7 @@ def update_profile(request):
             profile.picture = data['picture']
             profile.save()
 
-            return redirect('update_profile')
+            return redirect('users:update_profile')
     else:
         form = ProfileForm()
 
@@ -48,7 +48,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user:
             login(request, user)
-            return redirect('feed')
+            return redirect('posts:feed')
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
 
@@ -58,7 +58,7 @@ def login_view(request):
 def logout_view(request):
     """Logout a user."""
     logout(request)
-    return redirect('login')
+    return redirect('users:login')
 
 def signup(request):
     """Sign up view."""
@@ -66,7 +66,7 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('users:login')
     else:
         form = SignupForm()
     
